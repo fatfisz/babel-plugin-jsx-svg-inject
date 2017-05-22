b803d0f47f3791e9e937271c05dd3cd354737913
fatfisz <fatfisz@gmail.com>
2017-05-23 00:49:00 +0200

    Separate the normalizeOpts function

--- src/index.js
+++ src/index.js
@@ -5,29 +5,30 @@ import babelPluginSyntaxJSX from 'babel-plugin-syntax-jsx';
 import JSXAttribute from './jsx-attribute-visitor';
 
 
+function normalizeOpts(opts) {
+  if (!opts.root) {
+    opts.root = '.';
+  }
+  opts.root = resolve(opts.root);
+
+  if (!opts.nameProp) {
+    opts.nameProp = 'svgName';
+  }
+
+  if (!opts.contentsProp) {
+    opts.contentsProp = 'svgContents';
+  }
+
+  opts.unwrap = Boolean(opts.unwrap);
+  opts.useImports = Boolean(opts.useImports);
+}
+
 export default function ({ types }) {
   return {
     pre() {
-      const { opts } = this;
-
       this.cache = new Map();
       this.types = types;
-
-      if (!opts.root) {
-        opts.root = '.';
-      }
-      opts.root = resolve(opts.root);
-
-      if (!opts.nameProp) {
-        opts.nameProp = 'svgName';
-      }
-
-      if (!opts.contentsProp) {
-        opts.contentsProp = 'svgContents';
-      }
-
-      opts.unwrap = Boolean(opts.unwrap);
-      opts.useImports = Boolean(opts.useImports);
+      normalizeOpts(this.opts);
     },
 
     inherits: babelPluginSyntaxJSX,
